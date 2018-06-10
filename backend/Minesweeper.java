173df41b9bbccb6769bc349bf43ea8affeb0071c
peej-s <peejmontero@gmail.com>
2018-06-10 18:40:55 -0400

    Added comments

--- backend/Minesweeper.java
+++ backend/Minesweeper.java
@@ -7,7 +7,7 @@ package backend;
 // Print: Winning Algorithm, incorrect input
 
 public class Minesweeper {
-	
+
 	GamePiece[][] gameMap;
 	GamePiece[][] surfaceGrid;
 	public int columns;
@@ -15,7 +15,7 @@ public class Minesweeper {
 	int bombs;
 	int cleared = 0;
 	boolean status = true; // True if game is active (user has not died)
-	
+
 	public Minesweeper(int columns, int rows, int bombs){
 		// Initialize Game
 		this.columns = columns;
@@ -25,11 +25,11 @@ public class Minesweeper {
 	}
 
 	// End Initializers
-	
+
 	private void setGrid(GamePiece[][] gameMap){
 		this.gameMap = gameMap;
 	}
-	
+
 	private void setSurfaceGrid(GamePiece[][] surfaceGrid){
 		this.surfaceGrid = surfaceGrid;
 	}
@@ -37,11 +37,11 @@ public class Minesweeper {
     private void setStatus(boolean status) {
 		this.status = status;
 	}
-    
+
 	public GamePiece[][] getGameMap(){
 		return this.gameMap;
 	}
-	
+
 	public GamePiece[][] getSurfaceGrid(){
 		return this.surfaceGrid;
 	}
@@ -49,46 +49,49 @@ public class Minesweeper {
 	public boolean getStatus() {
 		return this.status;
 	}
-	
+
 	public boolean getWin() {
 		return (this.columns * this.rows - this.bombs) == this.cleared;
-	}	
+	}
 
 	// End Getters/Setters
-	
+
 	private void updateSurfaceGrid(GamePiece piece, int column, int row){
 		this.surfaceGrid[column][row] = piece;
 	}
-	
+
 	public String makeMove(int column, int row){
+		// Compares a coordinate on a grid to the coordinate on the gamemap.
+		// If there is no surrounding bombs, then automatically "clicks" on surrounding spaces
+
 		if (getSurfaceGrid()[column][row] != null){
 			return "Already Selected\n";
 		}
 		GamePiece piece = getGameMap()[column][row];
 		int maxColumn = getGameMap().length;
 		int maxRow = getGameMap()[0].length;
-				
+
 	    updateSurfaceGrid(piece, column, row);
-		
+
 		if(piece.isBomb()){
 			setStatus(false);
-			return "Game Over";	
+			return "Game Over";
 		}
 		else{
 			this.cleared++;
 			if (((EmptyLand) piece).surroundings == 0){
 				for (int i = column-1; i <= column+1; i++){
 					for (int j = row-1; j <= row+1; j++){
-						if ((!(i == column && row == j)) 
-								&& (!(i < 0 || j < 0)) 
+						if ((!(i == column && row == j))
+								&& (!(i < 0 || j < 0))
 								&& (!(i >= maxColumn || j >= maxRow))){
-							makeMove(i, j);
+							makeMove(i, j); // Makes a move on all surrounding spots (that are within the grid)
 						}
 					}
 				}
 			}
 			return "Nice!";
-			
+
 		}
 	}
 
@@ -99,19 +102,19 @@ public class Minesweeper {
 		play();
 		return "Game Reset!";
 	}
-	
+
 	public String play(){
-		
+
     	GamePiece[][] gameMap = new GamePiece[this.columns][this.rows];
     	GamePiece[][] surfaceGrid = new GamePiece[this.columns][this.rows];
     	GameInitializer initializer = new GameInitializer();
-    	
+
     	initializer.populateGameMap(gameMap, this.bombs);
     	initializer.populateSurfaceGrid(surfaceGrid);
 
     	setGrid(gameMap);
     	setSurfaceGrid(surfaceGrid);
-    	
+
     	return "Game Started!";
     }
 
